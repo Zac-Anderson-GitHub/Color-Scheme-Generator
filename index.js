041071c303fd1baaf668496cabc0a1395100b06e
MichaelGrinstead <michael.a.grinstead@gmail.com>
2024-10-16 10:06:29 +1000

    feat: style footer

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-
-
 document.getElementById("fetch-scheme").addEventListener("click", () => {
+  const selectedColor = document
+    .getElementById("color-picker")
+    .value.substring(1);
 
-    const selectedColor = document.getElementById("color-picker").value.substring(1)
+  const schemeType = document.getElementById("scheme-options").value;
 
-    const schemeType = document.getElementById("scheme-options").value
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${schemeType}&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${schemeType}&count=5`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        
-        const schemeDisplay = document.getElementById("color-scheme-display")
-        schemeDisplay.innerHTML = ""
-        
-        const colorCodeDisplay = document.getElementById("footer-content")
-        colorCodeDisplay.innerHTML = ""
-        
-        data.colors.forEach(color => {
-            const colorDiv = document.createElement("div")
-            colorDiv.classList.add("color-block")
-            colorDiv.style.backgroundColor = color.hex.value
-            schemeDisplay.appendChild(colorDiv)
-            
-            const colorCode = document.createElement("div")
-            colorCode.classList.add("color-code")
-            colorCode.textContent = color.hex.value
-            colorCodeDisplay.appendChild(colorCode)
-            
-        })
+      const schemeDisplay = document.getElementById("color-scheme-display");
+      schemeDisplay.innerHTML = "";
 
-        
-    })
-    
-})
+      const colorCodeDisplay = document.getElementById("footer-content");
+      colorCodeDisplay.innerHTML = "";
+
+      data.colors.forEach((color) => {
+        const colorDiv = document.createElement("div");
+        colorDiv.classList.add("color-block");
+        colorDiv.style.backgroundColor = color.hex.value;
+        schemeDisplay.appendChild(colorDiv);
+
+        const colorCode = document.createElement("div");
+        colorCode.classList.add("color-code");
+        colorCode.textContent = color.hex.value;
+        colorCodeDisplay.appendChild(colorCode);
+      });
+    });
+});
